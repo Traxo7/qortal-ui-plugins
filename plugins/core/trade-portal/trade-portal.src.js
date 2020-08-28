2bcad84d189fab1e2c64982ae81eaecfa1d86946
LOTW <lotw7270@protonmail.com>
2020-08-28 02:31:56 +0100

    NEW: Remove Stuck Offers

--- plugins/core/trade-portal/trade-portal.src.js
+++ plugins/core/trade-portal/trade-portal.src.js
@@ -6,6 +6,7 @@ import { Epml } from '../../../epml.js'
 import '@material/mwc-button'
 import '@material/mwc-textfield'
 import '@material/mwc-icon-button'
+import '@material/mwc-dialog'
 import '@polymer/paper-spinner/paper-spinner-lite.js'
 import '@vaadin/vaadin-grid/vaadin-grid.js'
 import '@vaadin/vaadin-grid/vaadin-grid-sorter'
@@ -31,6 +32,9 @@ class TradePortal extends LitElement {
             myHistoricTrades: { type: Array },
             tradeOffersSocketCounter: { type: Number },
             cancelBtnDisable: { type: Boolean },
+            myOfferingOrders: { type: Array },
+            openTradeOrders: { type: Array },
+            cancelStuckOfferBtnDisable: { type: Boolean }
         }
     }
 
@@ -217,9 +221,9 @@ class TradePortal extends LitElement {
             @media(min-width:701px) {
 
                 * {
-                    margin: 0;
+                    /* margin: 0;
                     padding: 0;
-                    box-sizing: border-box;
+                    box-sizing: border-box; */
                 }
 
                 #trade-portal {
@@ -288,6 +292,9 @@ class TradePortal extends LitElement {
         this.myHistoricTrades = []
         this.tradeOffersSocketCounter = 0
         this.cancelBtnDisable = false
+        this.myOfferingOrders = []
+        this.openTradeOrders = []
+        this.cancelStuckOfferBtnDisable = false
     }
 
     // TODO: Spllit this large chunk of code into individual components
@@ -311,7 +318,9 @@ class TradePortal extends LitElement {
                         </div>
                         <div class="open-trades">
                             <div class="box">
-                                <header>OPEN MARKET SELL ORDERS</header>
+                                <header>
+                                    <span>OPEN MARKET SELL ORDERS</span>
+                                </header>
                                 <div class="border-wrapper">
                                     <vaadin-grid theme="compact column-borders row-stripes wrap-cell-content" id="openOrdersGrid" aria-label="Open Orders" .items="${this.openOrders}">
                                         <vaadin-grid-column resizable header="Amount (QORT)" path="qortAmount"></vaadin-grid-column>
@@ -485,7 +494,10 @@ class TradePortal extends LitElement {
                     <div id="third-trade-section">
                         <div class="my-open-orders">
                             <div class="box">
-                                <header>MY ORDERS</header>
+                                <header>
+                                    <span>MY ORDERS</span>
+                                    <mwc-icon-button icon="more_vert" @click=${() => this.showStuckOrdersDialog()}></mwc-icon-button>
+                                </header>
                                 <div class="border-wrapper">
                                     <vaadin-grid theme="compact column-borders row-stripes wrap-cell-content" id="myOrdersGrid" aria-label="My Orders" .items="${this.myOrders}">
                                         <vaadin-grid-column resizable header="Date" .renderer=${(root, column, data) => {
@@ -544,6 +556,33 @@ class TradePortal extends LitElement {
                 </div>
 
             </div>
+            
+            <!-- Manage Stuck Orders Dialog -->
+            <mwc-dialog id="manageStuckOrdersDialog" scrimClickAction="${this.cancelStuckOfferBtnDisable ? '' : 'close'}">
+                <div style="text-align:center">
+                    <h1>Stuck Offers</h1>
+                    <hr>
+                </div>
+
+                <div>
+                    <vaadin-grid style="width: 500px" theme="compact column-borders row-stripes wrap-cell-content" id="stuckOrdersGrid" aria-label="My Offering Orders" .items="${this.myOfferingOrders}">
+                            <vaadin-grid-column resizable header="Amount (QORT)" path="qortAmount"></vaadin-grid-column>
+                            <vaadin-grid-column resizable header="Price (BTC)" path="priceBtc" ></vaadin-grid-column>
+                            <vaadin-grid-column resizable header="Total (BTC)" path="btcAmount"></vaadin-grid-column>
+                            <vaadin-grid-column resizable width="5rem" flex-grow="0" header="Action" .renderer=${(root, column, data) => {
+                render(html`${this.renderCancelStuckOfferButton(data.item)}`, root)
+            }}></vaadin-grid-column>
+                    </vaadin-grid>
+                </div>
+
+                <mwc-button
+                    ?disabled="${this.cancelStuckOfferBtnDisable}"
+                    slot="secondaryAction"
+                    dialogAction="cancel"
+                    class="cancel">
+                    Close
+                </mwc-button>
+            </mwc-dialog>
         `
     }
 
@@ -564,6 +603,7 @@ class TradePortal extends LitElement {
         this.myOrdersGrid = this.shadowRoot.getElementById('myOrdersGrid')
         this.historicTradesGrid = this.shadowRoot.getElementById('historicTradesGrid')
         this.myHistoricTradesGrid = this.shadowRoot.getElementById('myHistoricTradesGrid')
+        this.stuckOrdersGrid = this.shadowRoot.getElementById('stuckOrdersGrid')
 
         // call getOpenOrdersGrid
         this.getOpenOrdersGrid()
@@ -589,8 +629,8 @@ class TradePortal extends LitElement {
         let configLoaded = false
         parentEpml.ready().then(() => {
 
-            // Init Socket
-            this.initSocket()
+            // Create Trade Portal Connection
+            this.createConnection()
 
             parentEpml.subscribe('selected_address', async selectedAddress => {
                 this.selectedAddress = {}
@@ -646,22 +686,19 @@ class TradePortal extends LitElement {
 
     processOfferingTrade(offer) {
 
-        // console.log(this.openOrdersGrid.columns);
-
         const offerItem = {
             ...offer,
             priceBtc: this.round(parseFloat(offer.btcAmount) / parseFloat(offer.qortAmount))
         }
 
         const addOffer = () => {
-            this.openOrdersGrid.items.unshift(offerItem)
+            this.openOrdersGrid.items.unshift(offerItem);
             this.openOrdersGrid.clearCache();
         }
 
         const initOffer = () => {
-
-            this.openOrdersGrid.items.push(offerItem)
-            this.openOrdersGrid.clearCache()
+            this.openOrdersGrid.items.push(offerItem);
+            this.openOrdersGrid.clearCache();
         }
 
         this.openOrdersGrid.items.length === 0 ? initOffer() : addOffer()
@@ -756,6 +793,14 @@ class TradePortal extends LitElement {
                 this.openOrdersGrid.clearCache()
             }
         })
+
+        this.stuckOrdersGrid.items.forEach((item, index) => {
+            if (item.qortalAtAddress === offer.qortalAtAddress) {
+
+                this.stuckOrdersGrid.items.splice(index, 1)
+                this.stuckOrdersGrid.clearCache()
+            }
+        })
     }
 
     processTradeOffers(offers) {
@@ -769,19 +814,23 @@ class TradePortal extends LitElement {
          */
 
         offers.forEach(offer => {
-            if (offer.mode == 'OFFERING') {
 
-                this.processOfferingTrade(offer)
-            } else if (offer.mode == 'REDEEMED') {
+            if (offer.mode === 'OFFERING') {
+
+                if (this.tradeOffersSocketCounter > 1) {
+
+                    this.processOfferingTrade(offer)
+                }
+            } else if (offer.mode === 'REDEEMED') {
 
                 this.processRedeemedTrade(offer)
-            } else if (offer.mode == 'TRADING') {
+            } else if (offer.mode === 'TRADING') {
 
                 this.processTradingTrade(offer)  // Haha Trading Trade (^_^)
-            } else if (offer.mode == 'REFUNDED') {
+            } else if (offer.mode === 'REFUNDED') {
 
                 this.processRefundedTrade(offer)
-            } else if (offer.mode == 'CANCELLED') {
+            } else if (offer.mode === 'CANCELLED') {
 
                 this.processCancelledTrade(offer)
             }
@@ -818,8 +867,6 @@ class TradePortal extends LitElement {
      */
 
     processTradeBotStates(states) {
-
-        // console.log('TRADE BOT STATES: ', states);
 
         /** TRADEBOT STATES
          *  - BOB_WAITING_FOR_AT_CONFIRM
@@ -886,6 +933,9 @@ class TradePortal extends LitElement {
                 }
             }
         })
+
+        // Filter Stuck Trades
+        this.filterStuckTrades(states)
     }
 
 
@@ -935,25 +985,54 @@ class TradePortal extends LitElement {
         })
     }
 
+
+    processGetOffers(offers) {
+
+        const offerItem = (offer) => {
+            return {
+                ...offer,
+                btcAmount: offer.expectedBitcoin,
+                priceBtc: this.round(parseFloat(offer.expectedBitcoin) / parseFloat(offer.qortAmount))
+            }
+        }
+
+        const addOffer = (offer) => {
+            this.openOrdersGrid.items.unshift(offerItem(offer))
+            this.openOrdersGrid.clearCache();
+        }
+
+        const initOffer = (offer) => {
+            this.openOrdersGrid.items.push(offerItem(offer))
+            this.openOrdersGrid.clearCache()
+        }
+
+        const handleOffers = () => {
+            offers.forEach(offer => {
+                this.openOrdersGrid.items.length === 0 ? initOffer(offer) : addOffer(offer)
+            })
+        }
+
+        handleOffers()
+    }
+
+
     initSocket() {
+
+        const getOffers = async () => {
+
+            const url = `http://NODEURL/crosschain/tradeoffers`
+            const res = await fetch(url)
+            const _openTradeOrders = await res.json()
+            const openTradeOrders = await _openTradeOrders.filter(order => order.mode === "OFFERING")
+            self.postMessage({ type: "GET_OFFERS", data: openTradeOrders });
+        }
 
         const initTradeOffersWebSocket = () => {
 
+            let tradeOffersSocketCounter = 0
             let socketTimeout
 
-            let myNode = window.parent.reduxStore.getState().app.nodeConfig.knownNodes[window.parent.reduxStore.getState().app.nodeConfig.node]
-            let nodeUrl = myNode.domain + ":" + myNode.port
-
-            let socketLink
-
-            if (window.parent.location.protocol === "https:") {
-
-                socketLink = `wss://${nodeUrl}/websockets/crosschain/tradeoffers?includeHistoric=true`;
-            } else {
-
-                // Fallback to http
-                socketLink = `ws://${nodeUrl}/websockets/crosschain/tradeoffers?includeHistoric=true`;
-            }
+            let socketLink = `ws://NODEURL/websockets/crosschain/tradeoffers?includeHistoric=true`;
 
             const socket = new WebSocket(socketLink);
 
@@ -961,14 +1040,15 @@ class TradePortal extends LitElement {
             socket.onopen = () => {
 
                 setTimeout(pingSocket, 50)
-                this.tradeOffersSocketCounter += 1
+                tradeOffersSocketCounter += 1
                 console.log(`[TRADE-OFFERS-SOCKET] ==>: CONNECTED`);
             }
 
             // Message Event
             socket.onmessage = (e) => {
 
-                this.processTradeOffers(JSON.parse(e.data))
+                self.postMessage({ type: "TRADE_OFFERS", data: e.data, counter: tradeOffersSocketCounter });
+                tradeOffersSocketCounter += 1
             }
 
             // Closed Event
@@ -998,19 +1078,7 @@ class TradePortal extends LitElement {
 
             let socketTimeout
 
-            let myNode = window.parent.reduxStore.getState().app.nodeConfig.knownNodes[window.parent.reduxStore.getState().app.nodeConfig.node]
-            let nodeUrl = myNode.domain + ":" + myNode.port
-
-            let socketLink
-
-            if (window.parent.location.protocol === "https:") {
-
-                socketLink = `wss://${nodeUrl}/websockets/crosschain/tradebot`;
-            } else {
-
-                // Fallback to http
-                socketLink = `ws://${nodeUrl}/websockets/crosschain/tradebot`;
-            }
+            let socketLink = `ws://NODEURL/websockets/crosschain/tradebot`;
 
             const socket = new WebSocket(socketLink);
 
@@ -1024,7 +1092,7 @@ class TradePortal extends LitElement {
             // Message Event
             socket.onmessage = (e) => {
 
-                this.processTradeBotStates(JSON.parse(e.data))
+                self.postMessage({ type: "TRADE_BOT", data: e.data });
             }
 
             // Closed Event
@@ -1059,6 +1127,9 @@ class TradePortal extends LitElement {
 
             setTimeout(() => initTradeBotWebSocket(), 3000)
         }
+
+        // Get Offers
+        getOffers()
 
         // Start TradeOffersWebSocket
         initTradeOffersWebSocket()
@@ -1261,6 +1332,57 @@ class TradePortal extends LitElement {
         }
     }
 
+    showStuckOrdersDialog() {
+
+        this.shadowRoot.querySelector('#manageStuckOrdersDialog').show()
+    }
+
+    async cancelStuckOfferAction(offer) {
+
+        this.cancelStuckOfferBtnDisable = true
+
+        const makeRequest = async () => {
+
+            const response = await parentEpml.request('deleteTradeOffer', {
+                creatorPublicKey: this.selectedAddress.base58PublicKey,
+                atAddress: offer.qortalAtAddress
+            })
+
+            return response
+        }
+
+        const manageResponse = (response) => {
+
+            if (response === true) {
+
+                this.cancelStuckOfferBtnDisable = false
+                parentEpml.request('showSnackBar', "Trade Cancelling In Progress!");
+            } else if (response === false) {
+
+                this.cancelStuckOfferBtnDisable = false
+                parentEpml.request('showSnackBar', "Failed to Cancel Trade. Try again!");
+            } else {
+
+                this.cancelStuckOfferBtnDisable = false
+                parentEpml.request('showSnackBar', `Failed to Cancel Trade. ERROR_CODE: ${response.message}`);
+            }
+        }
+
+        // Call makeRequest
+        const res = await makeRequest()
+        manageResponse(res)
+
+    }
+
+
+    renderCancelStuckOfferButton(offerItem) {
+
+        if (offerItem.mode === 'OFFERING' && offerItem.qortalCreator === this.selectedAddress.address) {
+            return html`<mwc-button id="offer-${offerItem.qortalAtAddress}" ?disabled=${this.cancelStuckOfferBtnDisable} class="cancel" @click=${(e) => this.cancelStuckOfferAction(offerItem)}>CANCEL</mwc-button>`
+        } else {
+            return ''
+        }
+    }
 
     // Helper Functions (Re-Used in Most part of the UI )
     _checkSellAmount(e) {
@@ -1454,6 +1576,127 @@ class TradePortal extends LitElement {
         let result = (Math.round(parseFloat(number) * 1e8) / 1e8).toFixed(8)
         return result
     }
+
+    inlineWorker(passedFunction, modifiers) {
+
+        let parsedFunction = ``
+
+        modifiers.forEach(modifier => {
+            let regex = new RegExp(modifier.searchValue, 'g')
+            parsedFunction = parsedFunction.length === 0 ? `(function ${passedFunction.toString().trim().replace(regex, modifier.replaceValue)})()` : parsedFunction.toString().trim().replace(regex, modifier.replaceValue)
+        })
+
+        const workerUrl = URL.createObjectURL(new Blob([parsedFunction], { type: "text/javascript" }));
+        const worker = new Worker(workerUrl);
+        URL.revokeObjectURL(workerUrl);
+        return worker;
+    }
+
+    createConnection() {
+
+        const handleMessage = (message) => {
+
+            switch (message.type) {
+                case "GET_OFFERS":
+                    return this.processGetOffers(message.data)
+                case "TRADE_OFFERS":
+                    this.tradeOffersSocketCounter = message.counter
+                    return this.processTradeOffers(JSON.parse(message.data))
+                case "TRADE_BOT":
+                    return this.processTradeBotStates(JSON.parse(message.data))
+                default:
+                    break;
+            }
+        }
+
+        const modifiers = [
+            { searchValue: 'NODEURL', replaceValue: 'localhost:12391' }
+        ]
+
+        const connectedWorker = this.inlineWorker(this.initSocket, modifiers)
+        connectedWorker.addEventListener('message', function (event) {
+            handleMessage(event.data)
+        })
+    }
+
+    handleStuckTrades() {
+
+        let tradeBotStates = []
+
+        self.addEventListener('message', function (event) {
+            tradeBotStates = event.data
+        })
+
+        const filterStuckOffers = (myOffers) => {
+
+            const myTradeBotStates = tradeBotStates.filter(state => state.creatorAddress === 'SELECTED_ADDRESS');
+
+            const stuckOffers = myOffers.filter(myOffer => {
+                let value = myTradeBotStates.some(myTradeBotState => myOffer.qortalAtAddress === myTradeBotState.atAddress)
+                return !value
+            });
+            return stuckOffers;
+        }
+
+        const getOffers = async () => {
+
+            const url = `http://NODEURL/crosschain/tradeoffers`
+            const res = await fetch(url)
+            const openTradeOrders = await res.json()
+            const myOpenTradeOrders = await openTradeOrders.filter(order => order.mode === "OFFERING" && order.qortalCreator === "SELECTED_ADDRESS");
+
+            const stuckOffers = filterStuckOffers(myOpenTradeOrders)
+            self.postMessage({ type: "STUCK_OFFERS", data: stuckOffers });
+        }
+
+        // Get Offers
+        getOffers()
+    }
+
+    filterStuckTrades(states) {
+
+        const handleMessage = (message) => {
+
+            const offers = message.data
+
+            const offerItem = (offer) => {
+                return {
+                    ...offer,
+                    btcAmount: offer.expectedBitcoin,
+                    priceBtc: this.round(parseFloat(offer.expectedBitcoin) / parseFloat(offer.qortAmount))
+                }
+            }
+
+            const addStuckOrders = (offerItem) => {
+
+                if (offerItem.qortalCreator === this.selectedAddress.address) {
+                    this.stuckOrdersGrid.items.unshift(offerItem)
+                    this.stuckOrdersGrid.clearCache();
+                }
+            }
+
+            const handleOffers = () => {
+                offers.forEach(offer => {
+                    addStuckOrders(offerItem(offer))
+                })
+            }
+
+            handleOffers()
+        }
+
+        const modifiers = [
+            { searchValue: 'NODEURL', replaceValue: 'localhost:12391' },
+            { searchValue: 'SELECTED_ADDRESS', replaceValue: this.selectedAddress.address }
+        ]
+
+        const connectedWorker = this.inlineWorker(this.handleStuckTrades, modifiers)
+        connectedWorker.postMessage(states)
+        connectedWorker.addEventListener('message', function (event) {
+            handleMessage(event.data)
+            connectedWorker.terminate()
+        })
+    }
+
 }
 
 window.customElements.define('trade-portal', TradePortal)
